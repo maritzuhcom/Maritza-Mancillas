24dfff487ff2eb9a76f3dca2cbf552618bff71a4
Maritza Mancillas <mancillas.maritza@gmail.com>
2017-08-15 17:09:08 -0700

    aditional styles

--- js/logic.js
+++ js/logic.js
@@ -13,26 +13,35 @@ function init () {
   var resumeButton = document.body.querySelector('.resumeButton');
   var contactButton = document.body.querySelector('.contactButton');
   var middle = document.body.querySelector('.middle');
-  var arrow = document.body.querySelector('[data-ui~=arrow]')
+  var arrow = document.body.querySelector('[data-ui~=arrow]');
 
   bioButton.addEventListener('click', (e) => {
     e.preventDefault();
     middle.classList.add('showBio');
     arrow.classList.remove('hidden');
   });
+
   workButton.addEventListener('click', (e) => {
+    e.preventDefault();
     middle.classList.add('showWork');
     arrow.classList.remove('hidden');
   });
+
   resumeButton.addEventListener('click', (e) => {
+    e.preventDefault();
     middle.classList.add('showResume');
     arrow.classList.remove('hidden');
   });
+
   contactButton.addEventListener('click', (e) => {
+    e.preventDefault();
     middle.classList.add('showContact');
     arrow.classList.remove('hidden');
   });
+
   arrow.addEventListener('click', (e) => {
-    console.log('hello');
+    e.preventDefault();
+    middle.className = 'middle';
+    arrow.classList.add('hidden');
   });
 }
