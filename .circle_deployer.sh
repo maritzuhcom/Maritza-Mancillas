5316623ad4a40c5c80c1eeca1c2edc4abb2170bd
Maritza Mancillas <mancillas.maritza@gmail.com>
2019-01-15 23:04:55 -0800

    Update .circle_deployer.sh

--- .circle_deployer.sh
+++ .circle_deployer.sh
@@ -4,4 +4,4 @@ npm i --only=dev &&
 npm run build &&
 sudo npm i -g forever &&
 forever stopall &&
-TWILIO_ACCOUNT_ID=$TWILIO_ACCOUNT_ID TWILIO_AUTH_TOKEN=$TWILIO_AUTH_TOKEN NODE_ENV=production PORT=80 forever start build/bundle.js
+TWILIO_ACCOUNT_ID=$TWILIO_ACCOUNT_ID TWILIO_AUTH_TOKEN=$TWILIO_AUTH_TOKEN PORT=80 node build/bundle.js
