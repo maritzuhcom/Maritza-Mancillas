7197fefd4bf95061fdfcb260a8a33142a6f3272f
Maritza Mancillas <mancillas.maritza@gmail.com>
2019-01-16 19:19:57 -0800

    hmm

--- .circle_deployer.sh
+++ .circle_deployer.sh
@@ -2,9 +2,10 @@ rm -rf /node_modules
 npm i
 npm i --only=dev
 npm run build
-export NODE_ENV=production &&
-export TWILIO_AUTH_TOKEN=$TWILIO_AUTH_TOKEN &&
-export TWILIO_ACCOUNT_ID=$TWILIO_ACCOUNT_ID &&
-echo $TWILIO_ACCOUNT_ID &&
-echo $TWILIO_AUTH_TOKEN &&
+source /root/.bashrc
+export NODE_ENV=production
+echo "****variables****"
+echo $TWILIO_ACCOUNT_ID
+echo $TWILIO_AUTH_TOKEN
+echo "****variables****"
 docker-compose -f docker-compose.prod.yml up -d
